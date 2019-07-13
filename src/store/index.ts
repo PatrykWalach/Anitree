6284623faf0889c8dcc0b74ce09ca0ff8ac6e7bd
Patryk Wałach <patryk_walach@outlook.com>
2019-07-13 21:21:35 +0200

    Release 0.5.10

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,11 +7,15 @@ Vue.use(Vuex)
 import { ModuleMedia } from './modules/media'
 import { ModuleTitle } from './modules/title'
 import { ModuleAuth } from './modules/auth'
+import { ModuleSubmit } from './modules/submit'
+import { ModuleEdit } from './modules/edit'
 
 export interface RootState {
   media: ModuleMedia
   title: ModuleTitle
   auth: ModuleAuth
+  edit: ModuleEdit
+  submit: ModuleSubmit
 }
 
 export default new Vuex.Store<RootState>({})
