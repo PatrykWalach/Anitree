58e70ed312badd301b088d6c025de6df40bf6454
Patryk Wałach <patryk_walach@outlook.com>
2019-09-10 19:59:17 +0200

    feat(eslintrc): add sort-keys and sort-imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,24 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    '@typescript-eslint/no-unused-vars': 1
-  },
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+        vars: 'all',
+        varsIgnorePattern: '^h$',
+        ignoreRestSiblings: true
+      }
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single']
+      }
+    ],
+    'sort-keys': 'error'
   }
 }
