29a4d2d548f41c15fcfdad3d2ec00a41af89247f
Patryk Wałach <patryk_walach@outlook.com>
2020-01-19 13:36:21 +0100

    chore: use gradient

--- src/components/TheGridCardCover.vue
+++ src/components/TheGridCardCover.vue
@@ -1,23 +1,35 @@
 <template>
-  <media-card-cover :x="1" aspect-ratio="1" :media="media">
-    <v-overlay v-if="hover" absolute></v-overlay>
-  </media-card-cover>
+  <MediaCardCover :x="1" aspect-ratio="1" :media="media">
+    <VFadeTransition>
+      <div class="fill-height" v-show="hover" :style="{ backgroundImage }" />
+    </VFadeTransition>
+  </MediaCardCover>
 </template>
 
 <script lang="ts">
-import { createComponent } from '@vue/composition-api'
+import { createComponent, computed } from '@vue/composition-api'
 import { TheGridCardCover_media } from './__generated__/TheGridCardCover_media'
 import MediaCardCover from './MediaCardCover.vue'
 
 export interface Props {
   media: TheGridCardCover_media
-
   hover: boolean
 }
 
 export default createComponent<Readonly<Props>>({
   components: { MediaCardCover },
-
+  setup() {
+    const backgroundImage = computed(
+      () =>
+        `linear-gradient(to top, 
+      rgba(0, 0, 0, 0) 75%,
+      rgba(0, 0, 0, .5) 100%)
+        `,
+    )
+    return {
+      backgroundImage,
+    }
+  },
   props: {
     hover: { default: false, required: true, type: Boolean },
     media: {
