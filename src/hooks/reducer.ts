79f2b1f1325faeeec94fa6032e7eb18f80a5f838
Patryk Wałach <patryk_walach@outlook.com>
2020-01-19 12:39:06 +0100

    fix: improve useReducer middleware

--- src/hooks/reducer.ts
+++ src/hooks/reducer.ts
@@ -1,5 +1,14 @@
 import { Ref, computed, ref } from '@vue/composition-api'
 
+interface Store<A, S> {
+  dispatch: (action?: A) => void
+  state: Ref<Readonly<S>>
+}
+
+type Middlewares<A, S> = ((
+  store: Store<A, S>,
+) => (next: (action?: A) => void) => (action?: A) => any)[]
+
 interface UseReducer {
   <S>(reducer: (state: S) => S, initialState: S): [Ref<Readonly<S>>, () => void]
   <S, A>(reducer: (state: S, action: A) => S, initialState: S): [
@@ -9,39 +18,31 @@ interface UseReducer {
   <S, A>(
     reducer: (state: S, action: A) => S,
     initialState: S,
-    middlewares: ((
-      dispatch: (action?: A) => void,
-      immutableState: Ref<Readonly<S>>,
-    ) => (next: (action?: A) => void) => (action?: A) => any)[],
+    middlewares: Middlewares<A, S>,
   ): [Ref<Readonly<S>>, (action: A) => void]
 }
 
 export const useReducer: UseReducer = <S, A>(
   reducer: (state: S, action?: A) => S,
   initialState: S,
-  middlewares: ((
-    dispatch: (action?: A) => void,
-    immutableState: Ref<Readonly<S>>,
-  ) => (next: (action?: A) => void) => (action?: A) => any)[] = [],
+  middlewares: Middlewares<A, S> = [],
 ): [Ref<Readonly<S>>, (action?: A) => void] => {
   const state: Ref<S> = ref(initialState)
   const immutableState = computed(() => state.value)
 
-  const wrapper = {
+  const store: Store<A, S> = {
+    state: immutableState,
     dispatch(action?: A) {
       state.value = reducer(immutableState.value, action)
     },
   }
 
-  wrapper.dispatch = middlewares
+  store.dispatch = middlewares
     .slice()
     .reverse()
-    .reduce(
-      (next, middleware) => middleware(wrapper.dispatch, immutableState)(next),
-      wrapper.dispatch,
-    )
+    .reduce((next, middleware) => middleware(store)(next), store.dispatch)
 
-  return [immutableState, wrapper.dispatch]
+  return [immutableState, store.dispatch]
 }
 
 export const useComputedOrCallback = <T>(value: Ref<T> | (() => T)) => {
